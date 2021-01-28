5a5bc7ebab301ea5ea2de08ea80a42af1d492a18
sdcz-2442 <71774412+sdcz-2442@users.noreply.github.com>
2021-01-28 19:54:53 +0100

    First commit

---
+++ ArduinoDUAL/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace ArduinoDUAL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_connectar = new System.Windows.Forms.Button();
+            this.btn_configure = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_connectar
+            // 
+            this.btn_connectar.Location = new System.Drawing.Point(319, 34);
+            this.btn_connectar.Name = "btn_connectar";
+            this.btn_connectar.Size = new System.Drawing.Size(75, 23);
+            this.btn_connectar.TabIndex = 0;
+            this.btn_connectar.Text = "Connectar";
+            this.btn_connectar.UseVisualStyleBackColor = true;
+            this.btn_connectar.Click += new System.EventHandler(this.btn_connectar_Click);
+            // 
+            // btn_configure
+            // 
+            this.btn_configure.Location = new System.Drawing.Point(235, 26);
+            this.btn_configure.Name = "btn_configure";
+            this.btn_configure.Size = new System.Drawing.Size(75, 23);
+            this.btn_configure.TabIndex = 1;
+            this.btn_configure.Text = "Configurar";
+            this.btn_configure.UseVisualStyleBackColor = true;
+            this.btn_configure.Click += new System.EventHandler(this.btn_configure_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(53, 34);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(203, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.btn_configure);
+            this.panel1.Location = new System.Drawing.Point(53, 80);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(330, 115);
+            this.panel1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(92, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(92, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 255);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btn_connectar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_connectar;
+        private System.Windows.Forms.Button btn_configure;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ArduinoDUAL/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArduinoDUAL
+{
+    public partial class Form1 : Form
+    {
+        System.IO.Ports.SerialPort ArduinoPort;
+        public Form1()
+        {
+            InitializeComponent();
+
+            //crear Serial Port
+            ArduinoPort = new System.IO.Ports.SerialPort();
+            ArduinoPort.PortName = "COM4";  //sustituir por vuestro 
+            ArduinoPort.BaudRate = 9600;
+            ArduinoPort.Open();
+
+            //vincular eventos
+            this.FormClosing += FrmMain_FormClosing;
+            this.btn_configure.Click += btn_configure_Click;
+            this.btn_connectar.Click += btn_connectar_Click;
+        }
+
+        private void FrmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //cerrar puerto
+            if (ArduinoPort.IsOpen) ArduinoPort.Close();
+        }
+
+        private void btn_connectar_Click(object sender, EventArgs e)
+        {
+            ArduinoPort.Write("b");
+        }
+
+        private void btn_configure_Click(object sender, EventArgs e)
+        {
+            ArduinoPort.Write("a");
+        }
+    }
+}
